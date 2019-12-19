fcc1fdc6d1262567dfbe5ad73947f507be48f04b
nxgh <hgxn1101@gamil.com>
2019-12-19 23:13:35 +0800

    feat: :clown_face: 头像测试url

--- app/api/user/avatar.py
+++ app/api/user/avatar.py
@@ -1,9 +1,11 @@
 from flask_restful import Resource
 from flask import send_file
 
+from app.email import send_confirm_email
+from ..auth import generate_token
 
 
 class AvatarApi(Resource):
 
-    def get(self):
-        return send_file('avatars/anonymous.png')
+    def get(self, file_name):
+        return send_file('avatars/' + file_name)
